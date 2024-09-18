e2324f3cd2e981152f0feb3ad23ef8d6351a6f43
samdahboss <samuelolawumi65@gmail.com>
2024-09-18 22:49:13 +0100

    Added emailjs and configured it to enable email to be sent from contact us page

--- OFFICIAL-WEBSITE/src/components/Form.jsx
+++ OFFICIAL-WEBSITE/src/components/Form.jsx
@@ -1,4 +1,6 @@
 import { MdArrowForward, MdPerson, MdPhone, MdMail } from "react-icons/md";
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 
 // Form component
 export default function Form() {
@@ -7,24 +9,53 @@ export default function Form() {
     {
       icon: <MdPerson className="text-gray-500 lg:text-2xl" />,
       placeholder: "First Name",
+      name: "first_name",
     },
     {
       icon: <MdPerson className="text-gray-500 lg:text-2xl" />,
       placeholder: "Last Name",
+      name: "last_name",
     },
     {
       icon: <MdPhone className="text-gray-500 lg:text-2xl" />,
       placeholder: "Phone Number",
+      name: "user_phone",
     },
     {
       icon: <MdMail className="text-gray-500 lg:text-2xl" />,
       placeholder: "Email",
+      name: "user_email",
     },
   ];
 
+  const form = useRef();
+  const [emailSent, setEmailSent] = useState("");
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_v84wlm2", "template_n3bfm7c", form.current, {
+        publicKey: "FRzkYgrpiHA_BogGH",
+      })
+      .then(
+        () => {
+          setEmailSent("Email sent successfully!");
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+          setEmailSent("Failed to send email!");
+        }
+      );
+  };
+
   return (
     // Form container
-    <form className="w-full max-w-2xl lg:w-1/2 flex flex-col bg-[#253141] gap-4 mx-auto -mt-32 h-[461px] rounded-lg p-12">
+    <form
+      ref={form}
+      onSubmit={sendEmail}
+      className="w-full max-w-2xl lg:w-1/2 flex flex-col bg-[#253141] gap-4 mx-auto -mt-32 h-[461px] rounded-lg p-12"
+    >
       {/* Form title */}
       <h2 className="text-2xl sm:text-3xl text-white mx-2 font-medium">
         Connect with us
@@ -45,6 +76,8 @@ export default function Form() {
               placeholder={item.placeholder}
               key={index}
               className="outline-0 w-2/3 lg:text-sm text-xs"
+              name={item.name}
+              required
             />
           </div>
         ))}
@@ -54,13 +87,17 @@ export default function Form() {
       <textarea
         className="w-full h-1/2 p-2 rounded-lg outline-0 lg:text-sm text-xs"
         placeholder="Write your request"
+        name="message"
       />
-
-      {/* Submit button */}
-      <button className="ml-0 bg-[#F5901F] w-1/3 lg:w-1/4 text-white text-xs p-2 py-3 text-xs rounded-lg flex items-center gap-2">
-        Send Message
-        <MdArrowForward className="inline ml-auto" />
-      </button>
+      <div className="flex items-center">
+        {/* Submit button */}
+        <button type="submit" className="ml-0 bg-[#F5901F] w-1/3 lg:w-1/4 text-white text-xs p-2 py-3 text-xs rounded-lg flex items-center gap-2">
+          Send Message
+          <MdArrowForward className="inline ml-auto" />
+        </button>
+        <p className="text-white ml-auto">{emailSent}</p>
+      </div>
+      
     </form>
   );
 }
