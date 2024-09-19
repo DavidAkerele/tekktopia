f7ea14433ab20c552245ecd5854f9c2c335a4a7c
davidiscool <dammyadavid@gmail.com>
2024-09-19 11:20:07 +0100

    merged

--- OFFICIAL-WEBSITE/src/main.jsx
+++ OFFICIAL-WEBSITE/src/main.jsx
@@ -1,22 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import OurTeam from './pages/OurTeam'
-import ContactUs from './pages/ContactUs'
-import './index.css'
-import App from './App'
-import Blog from './pages/Blog'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import OurTeam from "./pages/OurTeam";
+import ContactUs from "./pages/ContactUs";
+import "./index.css";
+import App from "./App";
+import Blog from "./pages/Blog";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-  <BrowserRouter>
-  <Routes>
-    <Route path='/' element={<App/>}>
-    <Route path='/blog' element={<Blog/>}/>
-      <Route path='/our-team' element={<OurTeam/>}/>
-      <Route path='/contact-us' element={<ContactUs/>}/>
-    </Route>
-  </Routes>
-  </BrowserRouter>
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/our-team" element={<OurTeam />} />
+          <Route path="/contact-us" element={<ContactUs />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
