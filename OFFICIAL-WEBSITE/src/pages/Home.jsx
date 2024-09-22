8dc8aee6ec17c10d9eec9a6205106b9f1ca1b8c2
codewithfrancis <offor919@gmail.com>
2024-09-22 20:53:45 +0100

    home page

--- OFFICIAL-WEBSITE/src/pages/Home.jsx
+++ OFFICIAL-WEBSITE/src/pages/Home.jsx
@@ -7,8 +7,8 @@ const Home = () => {
     <section className="mt-24 md:mt-44 font-[Inter]">
       {/* First Home Section */}
       <div className="flex flex-col items-center min-h-screen px-4">
-        <div className="flex flex-col md:flex-row items-center justify-center gap-2">
-          <div className="text-center md:text-left leading-tight md:leading-[80px] font-medium text-[#0B0B0B] text-[36px] md:text-[60px] max-w-[906px]">
+        <div className="flex flex-col  md:flex-row items-center justify-center gap-2">
+          <div className="text-center  md:text-left leading-tight md:leading-[80px] font-medium text-[#0B0B0B] text-[28px] sm:text-[36px] md:text-[60px]">
             We manage your IT, so
           </div>
           <div className="flex items-center mt-4 md:mt-0">
@@ -26,11 +26,11 @@ const Home = () => {
           </div>
         </div>
 
-        <div className="text-center text-[36px] md:text-[60px] leading-tight md:leading-snug bg-gradient-to-r from-[#67BDD5] to-[#4B82CA] text-transparent font-medium bg-clip-text mt-4">
+        <div className="text-center text-[28px] sm:text-[36px] md:text-[60px] leading-tight md:leading-snug bg-gradient-to-r from-[#67BDD5] to-[#4B82CA] text-transparent font-medium bg-clip-text mt-4">
           you can manage your business.
         </div>
 
-        <p className="text-center text-[#697D95B2] text-[14px] md:text-[16px] mt-4 max-w-[865px]">
+        <p className="text-center text-[#697D95B2] text-[14px] sm:text-[16px] mt-4 max-w-[865px]">
           Welcome to TechTopia, where innovation meets expertise in the dynamic world of Information Technology. We are a forward-thinking IT company dedicated to delivering cutting-edge solutions tailored to meet the unique needs of our clients.
         </p>
 
@@ -52,13 +52,18 @@ const Home = () => {
       <section className="bg-[#081527] w-full">
         <div className="flex flex-col items-center min-h-screen px-4">
           <div className="mt-16 md:mt-[115px] text-center">
-            <div className="text-[#FFFFFF] text-[24px] md:text-[32px] font-[DM Sans] font-medium leading-[30px] md:leading-[45px] mb-2">
+            <div className="text-[#FFFFFF] text-[22px] sm:text-[24px] md:text-[32px] font-[DM Sans] font-medium leading-[28px] sm:leading-[30px] md:leading-[45px] mb-2">
               Bridging excellence and <br />
               innovation across diverse sectors.
             </div>
-            <div className="text-[#FFFFFFB2] text-center text-[14px] md:text-[16px] leading-[24px]">
-              Welcome to TechTopia, where innovation meets expertise in the dynamic world of Information Technology.<br/> We are a forward-thinking IT company dedicated to delivering cutting-edge solutions tailored to.
+            <div className="text-[#FFFFFFB2] text-center text-[14px] sm:text-[16px] leading-[24px]">
+              Welcome to TechTopia, where innovation meets expertise in the dynamic world of Information Technology.<br />
+              We are a forward-thinking IT company dedicated to delivering cutting-edge solutions tailored to.
             </div>
+          </div>
+          <div className='flex mt-16 items-center gap-[24px]'>
+           <img src="/frame1.png" className='rounded-md' alt="" />
+           <img src="/frame2.png" className='rounded-md' alt="" />
           </div>
         </div>
       </section>
