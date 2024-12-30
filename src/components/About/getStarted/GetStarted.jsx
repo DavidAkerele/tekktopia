52b586a5b3e2f414e7b29b5097f8b937874babc6
samdahboss <samuelolawumi65@gmail.com>
2024-12-30 19:55:19 +0100

    Update GetStarted component: replace placeholder titles and descriptions with tailored content to enhance clarity and relevance

--- src/components/About/getStarted/GetStarted.jsx
+++ src/components/About/getStarted/GetStarted.jsx
@@ -3,52 +3,56 @@ import GlobalIcon from '../../../icons/GlobalIcon';
 const GetStarted = () => {
   const items = [
     {
-      title: 'Sed ut perspiciatis under.',
+      title: 'Tailored Solutions',
       description:
-        'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium.',
+        'Customized strategies designed to meet your unique needs and goals',
     },
     {
-      title: 'Sed ut perspiciatis under.',
+      title: 'Seamless Integration',
       description:
-        'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium.',
+        'Effortless adoption and implementation for your business processes.',
     },
     {
-      title: 'Sed ut perspiciatis under.',
+      title: 'Data-Driven Insights',
       description:
-        'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium.',
+        'Harnessing analytics to empower informed decision-making',
     },
     {
-      title: 'Sed ut perspiciatis under.',
+      title: '24/7 Support ',
       description:
-        'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium.',
+        'Round-the-clock assistance to ensure smooth operations and reliability.',
     },
   ];
 
   return (
     <section className="mt-[200px] px-4 sm:px-8">
-      <div className="flex flex-col text-center justify-between items-center">
-        <div className="text-[32px] sm:text-[36px] max-w-[518px] text-center ">
+      <div className="flex flex-col lg:flex-row text-center justify-between items-center">
+        <div className="text-[32px] max-w-[518px] text-center lg:text-left">
           Bridging excellence and innovation across diverse sectors.
         </div>
-        <div className="max-w-[484px] text-[14px] text-[#697D95B2] text-center  mt-4 sm:mt-0">
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
-          ab illo inventore veritatis et quasi architecto beatae vitae dicta
-          sunt lorem ispum.
+        <div className="max-w-[484px] text-[14px] text-[#697D95B2] text-center sm:text-left mt-4 sm:mt-0">
+          Every client is unique, and so are their challenges. We’re committed
+          to providing personalized tech solutions that fit your specific needs,
+          ensuring you always have the tools to succeed.
         </div>
-        <div className="mt-6 sm:mt-0 text-[#0056b3] font-semibold cursor-pointer">
-          Get started
+        <div className="mt-6 sm:mt-16 text-[#0056b3] font-semibold cursor-pointer">
+          Get started {">"}
         </div>
       </div>
 
       <div className="flex flex-col sm:flex-row items-center justify-between mt-[112px] gap-8 sm:gap-16">
         {items.map((item, index) => (
-          <div className="flex flex-col items-center text-center sm:text-left max-w-[265px]" key={index}>
+          <div
+            className="flex flex-col items-center sm:items-start text-center sm:text-left max-w-[265px]"
+            key={index}
+          >
             <div className="h-[40px] w-[40px] border-[#CCCCCC4D] border-[1px] flex items-center justify-center rounded-md mb-4">
               <GlobalIcon />
             </div>
-            <div className="text-[16px] text-[#697D95]">{item.title}</div>
-            <div className="text-[14px] text-[#697D95B2]">{item.description}</div>
+            <div className="text-[16px] text-[#697D95] lg:text-left w-full">{item.title}</div>
+            <div className="text-[14px] text-[#697D95B2]">
+              {item.description}
+            </div>
           </div>
         ))}
       </div>
