4e4708cb35e822e4ee1c456fe6c8e14b53df3236
samdahboss <samuelolawumi65@gmail.com>
2024-12-30 17:24:04 +0100

    Update DiverseSectors component: change heading and paragraph text for improved messaging, and modify list items for clarity

--- src/components/home/diverseSectors/DiverseSectors.jsx
+++ src/components/home/diverseSectors/DiverseSectors.jsx
@@ -1,7 +1,7 @@
 import { FiChevronRight } from 'react-icons/fi';
 import { GrStatusGood } from 'react-icons/gr';
 
-import home3 from '../../../assets/images/home3.svg'
+import home3 from '../../../assets/images/home3.svg';
 
 const DiverseSectors = () => {
   return (
@@ -21,28 +21,31 @@ const DiverseSectors = () => {
         {/* Text Section */}
         <div className="text-center lg:text-left max-w-[900px] px-4">
           <h3 className="text-[28px] lg:text-[32px] text-[#0B0B0B] leading-[40px] lg:leading-[45px]">
-            Bridging excellence and innovation across diverse sectors.
+            Your Vision, Our Expertise
           </h3>
           <p className="mt-2 text-[14px] lg:text-[16px] text-[#697D95B2] font-[400] leading-[22px] lg:leading-[24px]">
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
-            ab illo inventore veritatis et quasi architecto beatae vitae dicta
-            sunt lorem ispum.
+            At Tekktopia, we don’t just deliver services; we build partnerships.
+            Our team of experts is dedicated to understanding your unique goals
+            and crafting solutions that align with your vision. Whether it{"'"}s
+            scaling your business, managing operations, or driving innovation,
+            you can count on us to be with you every step of the way.
           </p>
           <button className="text-base mt-4 font-medium flex items-center rounded-md px-4 py-2 border shadow-sm">
             Request a demo <FiChevronRight />
           </button>
           <div className="mt-14">
-            {Array(4)
-              .fill()
-              .map((_, index) => (
-                <div key={index}>
-                  <p className="pb-4 flex text-[16px] font-[400] items-center gap-4">
-                    <GrStatusGood className="text-[#A66928]" /> Poluptatem
-                    accusantium dolore laudantium, totam rem.
-                  </p>
-                </div>
-              ))}
+            {[
+              'Reliability',
+              'Integrity',
+              'Collaboration',
+              'Customer Satisfaction',
+            ].map((_, index) => (
+              <div key={index}>
+                <p className="pb-4 flex text-[16px] font-[400] items-center gap-4">
+                  <GrStatusGood className="text-[#A66928]" /> {_}
+                </p>
+              </div>
+            ))}
           </div>
         </div>
       </div>
