3f506dc74b5cbb42fe43d722796212febb6591d7
samdahboss <samuelolawumi65@gmail.com>
2024-12-30 17:16:08 +0100

    Refactor BridgingExcellence component: update text content, improve layout, and enhance readability

--- src/components/home/bridgingExcellence/BridgingExcellence.jsx
+++ src/components/home/bridgingExcellence/BridgingExcellence.jsx
@@ -1,10 +1,10 @@
 // import React from 'react';
-import innovation1 from '../../../assets/images/innovation1.png'
-import innovation2 from '../../../assets/images/innovation2.png'
+import innovation1 from '../../../assets/images/innovation1.png';
+import innovation2 from '../../../assets/images/innovation2.png';
 const BridgingExcellence = () => {
   return (
     <section className="bg-[#081527] w-full px-4 lg:px-16">
-      <div className="flex flex-col items-center ">
+      <div className="flex flex-col _s-center ">
         {/* Heading Section */}
         <div className="mt-16 md:mt-[115px] text-center">
           <div className="text-[#FFFFFF] text-[22px] sm:text-[24px] md:text-[32px] font-[DM Sans] font-medium leading-[28px] sm:leading-[30px] md:leading-[45px] mb-2">
@@ -12,16 +12,15 @@ const BridgingExcellence = () => {
             innovation across diverse sectors.
           </div>
           <div className="text-[#FFFFFFB2] text-center text-[14px] sm:text-[16px] leading-[24px]">
-            Welcome to TechTopia, where innovation meets expertise in the
-            dynamic world of Information Technology.
-            <br />
-            We are a forward-thinking IT company dedicated to delivering
-            cutting-edge solutions tailored to.
+            Empowering businesses to thrive with tailored solutions and
+            innovative technology. Our expertise spans multiple <br />
+            industries, ensuring that no matter your needs, we have the tools
+            and strategies to drive success.
           </div>
         </div>
 
         {/* Image Section with Overlay */}
-        <div className="flex flex-col lg:flex-row mt-16 items-center justify-center gap-[24px] h-auto w-full">
+        <div className="flex flex-col lg:flex-row mt-16 _s-center justify-center gap-[24px] h-auto w-full">
           <div className="w-full lg:w-[588px] h-auto relative">
             <img
               src={innovation1}
@@ -30,11 +29,12 @@ const BridgingExcellence = () => {
             />
             <div className="absolute top-0 w-full h-full bg-gradient-to-r from-[#231506]/90 via-[#372511]/90 to-[#554c3b]/0 text-white py-[80px] px-[40px]">
               <div className="max-w-[300px]">
-                <p className="text-[20px]">Businesses trying to grow</p>
+                <p className="text-[20px] font-medium">Businesses trying to grow</p>
                 <p className="mt-[8px] text-[14px]">
-                  Lorem ipsum dolor sit amet consectetur. Facilisi a mattis
-                  risus sit pellentesque turpis. Morbi et enim tempor ante nulla
-                  massa semper felis.
+                  Running a business is challenging enough without worrying
+                  about technology. That’s why we deliver customized solutions
+                  designed to meet your specific needs, because one size never
+                  fits all
                 </p>
               </div>
             </div>
@@ -48,11 +48,13 @@ const BridgingExcellence = () => {
             />
             <div className="absolute top-0 w-full h-full bg-gradient-to-r from-[#231506]/90 via-[#372511]/90 to-[#554c3b]/0 text-white py-[80px] px-[40px]">
               <div className="max-w-[300px]">
-                <p className="text-[20px]">Businesses trying to grow</p>
+                <p className="text-[20px] font-medium">
+                  Innovative Solutions That <br/> Work for You
+                </p>
                 <p className="mt-[8px] text-[14px]">
-                  Lorem ipsum dolor sit amet consectetur. Facilisi a mattis
-                  risus sit pellentesque turpis. Morbi et enim tempor ante nulla
-                  massa semper felis.
+                  At Tekktopia, creativity meets precision. We design, build,
+                  and implement technology solutions that transform challenges
+                  into opportunities. Let’s build a smarter future together
                 </p>
               </div>
             </div>
@@ -63,32 +65,45 @@ const BridgingExcellence = () => {
         <div className="flex flex-col lg:flex-row gap-16 mt-16 w-full justify-center">
           <div className="max-w-[600px] w-full">
             <div className="text-[32px] text-[#FFFFFF] font-[400]">
-              Bridging excellence and innovation
+              Why should you choose Tekktopia?
             </div>
-            <p className="w-full text-[#FFFFFFB2] leading-[24px] text-[14px] lg:text-[16px]">
-              Welcome to TechTopia, where innovation meets expertise in the
-              dynamic world of Information Technology. We are a forward-thinking
-              IT company dedicated to delivering cutting-edge solutions tailored
-              to.
-            </p>
+            <p className="w-full text-[#FFFFFFB2] leading-[24px] text-[14px] lg:text-[16px]"></p>
           </div>
 
-          <div className="flex flex-wrap gap-[90px] mb-24 w-full lg:w-[570px] justify-center">
-            {Array(4)
-              .fill()
-              .map((_, index) => (
-                <div key={index} className="flex-grow max-w-[280px]">
-                  <div className="text-[24px] mb-2 font-[500] text-[#C2813B]">
-                    5 Mins
-                  </div>
-                  <p className="text-[16px] text-[#FFFFFF] font-[500] mb-2">
-                    Setup
-                  </p>
-                  <p className="text-[12px] border-b-[0.05px] border-[#CCCCCC] pb-4 text-[#FFFFFFB2]">
-                    Welcome to TechTopia, <br /> where innovation meets
-                  </p>
+          <div className="grid lg:grid-cols-2 gap-[90px] mb-24 w-full justify-center">
+            {[
+              {
+                title: 'Scalability',
+                subtitle: '',
+                description: 'Easily grow with Tekktopia as your needs evolve.',
+              },
+              {
+                title: 'Reliability',
+                subtitle: '',
+                description: 'Count on consistent, high performance solutions.',
+              },
+              {
+                title: 'Customer Success ',
+                subtitle: '',
+                description:
+                  'We prioritize your goals to ensure long term satisfaction.',
+              },
+              {
+                title: '24/7 Support ',
+                subtitle: '',
+                description:
+                  'Access help anytime with our round the clock service.',
+              },
+            ].map((_, index) => (
+              <div key={index} className="flex-grow max-w-[400px]">
+                <div className="text-[24px] mb-2 font-bold text-[#C2813B]">
+                  {_.title}
                 </div>
-              ))}
+                <p className="text-[14px] border-b-[0.05px] border-[#CCCCCC] py-8 text-[#FFFFFF]">
+                  {_.description}
+                </p>
+              </div>
+            ))}
           </div>
         </div>
       </div>
