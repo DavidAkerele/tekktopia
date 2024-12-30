e8b67a30f9231d1ded632b5db26aed632014bcb1
samdahboss <samuelolawumi65@gmail.com>
2024-12-30 18:49:30 +0100

    Update SignupTrigger component: replace placeholder text with a focused message on technology innovation and add a company name for branding

--- src/components/SignupTrigger.jsx
+++ src/components/SignupTrigger.jsx
@@ -11,15 +11,9 @@ export default function SignupTrigger({ signUpRounded }) {
       />
       <div className="left-[50%] top-[50%] -translate-x-[50%] -translate-y-[50%] w-full max-w-6xl px-4 mx-auto absolute text-white gap-4">
         <p className="font-[500] hero text-center text-[40px] mb-[20px]">
-          To take a trivial example, which of us ever undertakes laborious
-          physical exercise
+          Dedicated to Innovating The Future of Technology
         </p>
-        <p className="text-center">
-          But I must explain to you how all this mistaken idea of denouncing
-          pleasure and praising pain was born and I will give you a complete
-          account of the system, and expound the actual teachings of the great
-          explorer of the truth,
-        </p>
+        <p className="text-center">Tekktopia</p>
       </div>
     </div>
   );
