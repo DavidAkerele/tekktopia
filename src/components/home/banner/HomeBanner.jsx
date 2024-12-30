ef076c78da813ba06a10082c833bcae0ae2ecf52
samdahboss <samuelolawumi65@gmail.com>
2024-12-30 17:30:28 +0100

    Update HomeBanner component: enhance text content for clarity and impact, and adjust layout positioning

--- src/components/home/banner/HomeBanner.jsx
+++ src/components/home/banner/HomeBanner.jsx
@@ -9,18 +9,17 @@ const HomeBanner = () => {
           className="w-full h-[450px] sm:h-[650px] object-cover"
           alt="TechTopia Banner"
         />
-        <div className="absolute   px-4 sm:px-8">
+        <div className="absolute left-0 px-4 sm:px-8">
           <p className="text-[14px] sm:text-[16px] font-[400] text-[#FFFFFF] mb-4">
-            Tekktopia
+            TEKKTOPIA
           </p>
-          <h3 className="text-[24px] sm:text-[32px] font-[700] text-[#FFFFFF] mb-4">
+          <h3 className="text-[24px] sm:text-[32px]  text-[#FFFFFF] mb-4">
             Bridging excellence and innovation
           </h3>
           <p className="max-w-[588px] text-[14px] sm:text-[16px] font-[400] text-[#FFFFFF] mb-4">
-            Welcome to TechTopia, where innovation meets expertise in the
-            dynamic world of Information Technology. We are a forward-thinking
-            IT company dedicated to delivering cutting-edge solutions tailored
-            to your needs.
+            “We are committed to delivering solutions that combine innovation
+            with practical results, helping our clients succeed in a constantly
+            evolving world”.
           </p>
           <button className="text-base sm:text-lg mt-10 bg-[#FFFFFF] text-[#6797D5] font-medium flex items-center rounded-md px-4 py-2 border shadow-sm">
             Contact us
