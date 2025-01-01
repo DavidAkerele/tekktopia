eae8c945904cc68dbc74254031045f6510b8343a
samdahboss <samuelolawumi65@gmail.com>
2025-01-01 20:04:02 +0100

    Update Blog component: replace article images and adjust article titles for improved content accuracy

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -25,41 +25,19 @@ function Blog() {
                 'AI content detection in the emerging information ecosystem',
             },
             {
-              image: 'article2.png',
+              image: 'article3.png',
               id: 2,
               title:
                 'How to deal with Big Tech power! The “ Big Tech Rah”, a new form of biopower in the digital age',
             },
             {
-              image: 'article3.png',
+              image: 'article8.png',
               id: 3,
               title:
                 'The urban-texh feedback loop: A surveillance and development data-walk in South Lake Union',
             },
-          ].map((article, index) => (
-            <Link
-              to={{ pathname: `/blog/${article.id}`, state: 'Water' }}
-              key={index}
-              className="space-y-2"
-            >
-              <img
-                src={`/${article.image}`}
-                alt="blog"
-                className="w-full h-auto object-cover"
-              />
-              <p className="w-full">{article.title}</p>
-              <span className="cursor-pointer mt-4">Read more -&gt;</span>
-            </Link>
-          ))}
-        </section>
-
-        <h2 className="text-xl font-normal">More Articles</h2>
-
-        {/* Second Article Section */}
-        <section className="grid grid-cols-1 md:grid-cols-3 gap-8 w-full">
-          {[
             {
-              image: 'article4.png',
+              image: 'article7.png',
               id: 4,
               title: 'Equity Structure and Business Innovation Performance',
             },
@@ -75,7 +53,13 @@ function Blog() {
               title: 'The rise and fall of Tech Clusters: A literature revirew',
             },
             {
-              image: 'article7.png',
+              image: 'article2.png',
+              id: 7,
+              title:
+                'How tech companies advance sustainability through artificial intelligence ',
+            },
+            {
+              image: 'article4.png',
               id: 7,
               title:
                 'How tech companies advance sustainability through artificial intelligence ',
@@ -92,9 +76,7 @@ function Blog() {
                 className="w-full h-auto object-cover"
               />
               <p className="w-full">{article.title}</p>
-              <span className="text-blue-600 cursor-pointer">
-                Read more -&gt;
-              </span>
+              <span className="cursor-pointer mt-4">Read more -&gt;</span>
             </Link>
           ))}
         </section>
