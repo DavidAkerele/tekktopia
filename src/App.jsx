6dc1d106ba6534294f6eaa6bd2557762b5059ccc
samdahboss <samuelolawumi65@gmail.com>
2025-01-11 13:25:03 +0100

    Add ScrollToTop component to reset scroll position on route change

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import 'leaflet/dist/leaflet.css';
 import SignupTrigger from './components/SignupTrigger';
+import ScrollToTop from './components/ScrollToTop'
 
 function App() {
   return (
     <div className="relative">
       <Navbar />
+      <ScrollToTop/>
       <main className="min-h-screen flex flex-col items-center w-full ">
         <Outlet />
       </main>
